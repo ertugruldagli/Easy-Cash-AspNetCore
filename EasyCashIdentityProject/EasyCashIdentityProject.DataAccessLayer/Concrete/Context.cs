c3ba610eb426b51dac3a60c3c422f3bcb7858f87
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-09 11:51:56 +0300

    the commir

--- EasyCashIdentityProject/EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
+++ EasyCashIdentityProject/EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,5 +16,9 @@ namespace EasyCashIdentityProject.DataAccessLayer.Concrete
         {
             optionsBuilder.UseSqlServer("server=ED-INTERN;intitial catalog=EasyCashDB; integrated Security=true");
         }
+
+        public DbSet<CustomerAccount> customerAccounts { get; set; }
+
+        public DbSet<AccountProcess> accountProcesses  { get; set; }
     }
 }
