6c9cdd03f3cd58221135ddd66826f06e165fc711
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-10 11:57:10 +0300

    some changes

--- EasyCashIdentityProject/EasyCashIdentityProject.BussinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
+++ EasyCashIdentityProject/EasyCashIdentityProject.BussinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -9,5 +9,21 @@ using System.Threading.Tasks;
 namespace EasyCashIdentityProject.BussinessLayer.ValidationRules.AppUserValidationRules
 {
     public class AppUserRegisterValidator: AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Lütfen boş bırakmayiniz..");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Lütfen boş bırakmayiniz..");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Lütfen boş bırakmayiniz..");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Lütfen boş bırakmayiniz..");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Lütfen boş bırakmayiniz..");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Lütfen boş bırakmayiniz..");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Lütfen boş bırakmayiniz..");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Lütfen boş bırakmayiniz..");
+            RuleFor(x => x.ConfirmPassword).Equal(y=>y.Password).WithMessage("Lütfen boş bırakmayiniz..");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen boş bırakmayiniz..");
+
+        }
+    
     }
 }
